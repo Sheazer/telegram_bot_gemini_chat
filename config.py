9a5bdf13e4ace5b2348974ddd6e7d676dd6217e7
module_18_2 <erjan03iopro@gmail.com>
2025-03-16 22:50:37 +0600

    fix env file

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ from dotenv import load_dotenv
 
 load_dotenv()  # Загружаем переменные из .env
 
-TG_TOKEN = os.getenv("TG_TOKEN")
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+TG_TOKEN = 'AIzaSyA4vZi85J8ByVHXfINfXub3eRcrmAgAiso'
+GEMINI_API_KEY = '7737861074:AAG1h_x5FrmzzhZR7WPX5Wn1jqr1Fq9LvP8'
 
 if not TG_TOKEN or not GEMINI_API_KEY:
     raise ValueError("Ошибка: TG_TOKEN и GEMINI_API_KEY не найдены в .env!")
